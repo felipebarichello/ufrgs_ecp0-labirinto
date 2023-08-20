a6837014b97ca51bdde7c861ecc3a4c3586f4f72
Felipe <felipewbarichello@gmail.com>
2023-08-19 23:32:15 -0300

    Rework das rotações
    + struct Angle
    + Edubot::get_angle()
    + Edubot::discrete_angle()
    + Edubot::set_angle
    + Edubot::adjust_sideways #Unused

--- edubot.cpp
+++ edubot.cpp
@@ -3,8 +3,32 @@
 #include <cmath>
 
 
+Angle::Angle(double value) {
+	while (value > 180.0) {
+		value -= 360.0;
+	}
+
+	while (value < -180.0) {
+		value += 360.0;
+	}
+	
+	this->value = value;
+}
+
+Angle::operator double() const {
+	return this->value;
+}
+
 double Edubot::get_distance(Sonar sonar) {
     	return getSonar((size_t)sonar);
+}
+
+double Edubot::get_angle() {
+	return -this->getTheta();
+}
+
+double Edubot::discrete_angle(unsigned int sections, double offset) {
+	return (this->get_angle() + offset) / sections * sections;
 }
 
 double Edubot::safe_advance(double base_speed) {
@@ -20,7 +44,40 @@ double Edubot::safe_advance(double base_speed) {
 	return front_distance;
 }
 
-void Edubot::safe_rotate(double angle) {
+void Edubot::safe_rotate(Angle angle) {
     	this->rotate(angle);
     	this->sleepMilliseconds(this->rotation_duration);
+}
+
+void Edubot::set_angle(Angle angle) {
+	Angle dangle = (Angle)angle - (Angle)this->get_angle();
+	this->safe_rotate(dangle);
+}
+
+void Edubot::adjust_sideways(Side side, double min_distance, double max_distance) {
+	Sonar side_sonar;
+	
+	switch (side) {
+		case Side::Left:
+			side_sonar = Sonar::Left;
+			break;
+			
+		case Side::Right:
+			side_sonar = Sonar::Right;
+			break;
+	}
+
+	while (true) {
+		double side_distance = get_distance(side_sonar);
+
+		if (side_distance < min_distance) {
+			this->set_angle(this->discrete_angle(4, -45));
+		} else if (side_distance > max_distance) {
+			this->set_angle(this->discrete_angle(4, -45));
+		} else {
+			break;
+		}
+
+		this->sleepMilliseconds(1);
+	}
 }
--- edubot.hpp
+++ edubot.hpp
@@ -5,6 +5,8 @@
 
 #pragma once
 
+#include "side.hpp"
+
 
 enum class Sonar {
     Left         = 0,
@@ -14,6 +16,15 @@ enum class Sonar {
     FrontRight   = 4,
     MidRight     = 5,
     Right        = 6
+};
+
+struct Angle {
+	double value;
+
+
+	Angle(double value);
+
+	operator double() const;
 };
 
 class Edubot : public EdubotLib {
@@ -28,11 +39,26 @@ class Edubot : public EdubotLib {
 		// Retorna a distancia do sonar em metros
 		// Uma abstração em cima de getSonar() que recebe um enum ao inves de um size_t
 		double get_distance(Sonar sonar);
+
+		// Retorna o ângulo que o robô forma 
+		// É o oposto de getTheta()
+		// Isso porque theta é positivo no sentido anti-horário, enquanto rotate() é positivo no horário
+		double get_angle();
+
+		// Retorna o ângulo mais próximo a uma das `sections` seções, que começam a ser medidas na (rotação incial do robô) + `offset`
+		double discrete_angle(unsigned int sections, double offset);
 		
 		// Mover, cuidando para não bater na parede
 		// Retorna a distância da frente
 		double safe_advance(double base_speed);
 		
 		// Rotacionar, esperando o tempo necessário
-		void safe_rotate(double angle);
+		void safe_rotate(Angle angle);
+
+		// Rotacionar o robô até um ângulo relativo à posição inicial do robô
+		void set_angle(Angle angle);
+
+		// Move o robô lateralmente, com cuidado para não bater, até a distância lateral do obstáculo desejada
+		// Bloqueia o resto do programa até o fim do ajuste
+		void adjust_sideways(Side side, double min_distance, double max_distance);
 };
