dcef5b36664c4f8c3c775d6f2994148da0adbaee
ClickerPixel <felipewbarichello@gmail.com>
2023-08-21 18:37:32 -0300

    Implementar MazeSolver::get_rotations()

--- maze_solver.cpp
+++ maze_solver.cpp
@@ -1,6 +1,10 @@
 #include "maze_solver.hpp"
 
 
+signed char MazeSolver::get_rotations() {
+	return this->rotations;
+}
+
 void MazeSolver::rotated(Side side) {
 	this->rotations += (signed char)side;
 }
--- maze_solver.hpp
+++ maze_solver.hpp
@@ -5,12 +5,16 @@
 
 class MazeSolver {
 	public:
+		signed char get_rotations();
+	
 		// Avisa que o robô virou à esquerda ou à direita, respectivamente
 		void rotated(Side side);
 
 		// Retorna se o robô deve seguir a parede (ou seguir reto)
 		bool should_follow();
 
+
 	private:
+		// Contador de rotações do Pledge
 		signed char rotations = 0;
 };
