27844ba6a072b8acc2f2172bfa3aa047204f54d0
Felipe Barichello <87396379+felipebarichello@users.noreply.github.com>
2023-09-11 13:21:31 -0300

    Arrumar encoding quebrado

--- edubot.hpp
+++ edubot.hpp
@@ -42,7 +42,7 @@ struct Angle {
 	Angle operator-=(Angle other);
 	Angle operator-();
 
-	// Retorna o ï¿½ngulo mais prï¿½ximo a uma das `sections` seï¿½ï¿½es
+	// Retorna o ângulo mais próximo a uma das `sections` seções
 	Angle discrete(unsigned int sections, Angle offset);
 	
 	// Transformar em [0, 360]
@@ -51,22 +51,22 @@ struct Angle {
 
 class Edubot : public EdubotLib {
 	public:
-		// Uma distï¿½ncia segura a partir da qual ï¿½ seguro andar em alta velocidade
+		// Uma distância segura a partir da qual é seguro andar em alta velocidade
 		double safe_distance = 1.8f;
 
-		// Tempo mï¿½ximo que o robï¿½ leva para rotacionar
+		// Tempo máximo que o robô leva para rotacionar
 		double rotation_duration = 2000;
 
 
 		Edubot();
 
 		// Retorna a distancia do sonar em metros
-		// Uma abstraï¿½ï¿½o em cima de getSonar() que recebe um enum ao inves de um size_t
+		// Uma abstração em cima de getSonar() que recebe um enum ao inves de um size_t
 		double get_distance(Sonar sonar);
 
-		// Retorna o ï¿½ngulo que o robï¿½ forma 
-		// ï¿½ o oposto de getTheta()
-		// Isso porque theta ï¿½ positivo no sentido anti-horï¿½rio, enquanto rotate() ï¿½ positivo no horï¿½rio
+		// Retorna o ângulo que o robô forma 
+		// É o oposto de getTheta()
+		// Isso porque theta é positivo no sentido anti-horário, enquanto rotate() é positivo no horário
 		Angle get_angle();
 		
 		// Mover, cuidando para nï¿½o bater na parede
@@ -76,7 +76,7 @@ class Edubot : public EdubotLib {
 		// Rotacionar, esperando o tempo necessï¿½rio
 		void safe_rotate(Angle angle);
 
-		// Rotacionar o robï¿½ atï¿½ um ï¿½ngulo relativo ï¿½ posiï¿½ï¿½o inicial do robï¿½
+		// Rotacionar o robô até um ângulo relativo até posição inicial do robô
 		void set_angle(Angle angle);
 
 
@@ -90,13 +90,13 @@ class Edubot : public EdubotLib {
 			// Ajusta o ângulo para intended_theta até entrar na tolerância
 			void adjust_angle_until(Angle tolerance);
 		
-			// Move o robï¿½ lateralmente, a fim de colocï¿½-lo no meio da pista atï¿½ a distï¿½ncia lateral do obstï¿½culo desejada
-			// Bloqueia o resto do programa atï¿½ o fim do ajuste
+			// Move o robô lateralmente, a fim de colocá-lo no meio da pista até a distância lateral do obstáculo desejada
+			// Bloqueia o resto do programa até o fim do ajuste
 			void center_self(double tolerance, double too_far, double fallback);
 
 		private:
-			// "Teta esperado" proveniente das rotaï¿½ï¿½es solicitadas do programa
-			// O robï¿½ utilizarï¿½ este ï¿½ngulo para counterar o drift
+			// "Teta esperado" proveniente das rotações solicitadas do programa
+			// O robô utilizará este ângulo para counterar o drift
 			Angle intended_theta = Angle(0);
 	#endif
 
@@ -109,7 +109,7 @@ class Edubot : public EdubotLib {
 			bool move(double speed);
 			
 		private:
-			// Em que instante o robï¿½ simulado irï¿½ realizar um drift
+			// Em que instante o robô simulado irá realizar um drift
 			std::chrono::time_point<std::chrono::high_resolution_clock> drift_instant;
 			
 	#endif
